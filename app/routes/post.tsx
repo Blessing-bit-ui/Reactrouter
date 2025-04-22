786de85d2201f6e4247cd6c896d46ba6d3bb5de9
Blessing-bit-ui <yembeblessing27@gmail.com>
2025-04-22 04:48:55 -0700

    access the dater using {loaderData}

--- app/routes/post.tsx
+++ app/routes/post.tsx
@@ -2,15 +2,19 @@ import type { Route } from "./+types/post";
 
 export async function loader({ params }: Route.LoaderArgs){
     const postId =params.postId
+    return { postId }
 
 }
 export async function action(){
 
 }
 
-
-export default function Post(){
+export default function Post( { loaderData }: Route.ComponentProps){
   return(
-    <div> </div>
-  )  
+    <div>
+        {""}
+        <p> Post Id :{loaderData.postId}</p>
+        
+     </div>
+  ); 
 }
